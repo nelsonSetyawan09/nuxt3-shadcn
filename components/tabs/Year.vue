abb45bf34f5f9c54a6e19f3ecac574cb926bcba2
nelsonSetyawan09 <nelsonlinux9@gmail.com>
2024-10-08 11:28:14 +0800

    tabs in transaction

---
+++ components/tabs/Year.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Hello Year!!</div>
+</template>
--- pages/transaction/index.vue
+++ pages/transaction/index.vue
@@ -1,3 +1,48 @@
+<script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const tabList = [
+  {
+    code: "today",
+    title: "Today",
+    component: resolveComponent("TabsToday"),
+  },
+  {
+    code: "week",
+    title: "Week",
+    component: resolveComponent("TabsWeek"),
+  },
+  {
+    code: "month",
+    title: "Month",
+    component: resolveComponent("TabsMonth"),
+  },
+  {
+    code: "year",
+    title: "Year",
+    component: resolveComponent("TabsYear"),
+  },
+];
+const tabDefault = "today";
+</script>
+
 <template>
-  <div>Transaction Menu!</div>
+  <Tabs :default-value="tabDefault">
+    <TabsList>
+      <TabsTrigger
+        v-for="(tab, index) in tabList"
+        :key="`tab-trigger-${index}`"
+        :value="tab.code"
+      >
+        {{ tab.title }}
+      </TabsTrigger>
+    </TabsList>
+    <TabsContent
+      v-for="(tab, index) in tabList"
+      :key="`tab-content-${index}`"
+      :value="tab.code"
+    >
+      <component :is="tab.component" />
+    </TabsContent>
+  </Tabs>
 </template>
